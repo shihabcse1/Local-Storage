c9f16cb43e19591f1ceab1467e4a280daefaf7cb
shihabcse1 <tomdshihab@gmail.com>
2022-03-08 10:40:46 +0600

    add item fucntion added

---
+++ shopping.js
@@ -0,0 +1,17 @@
+const addItem = () => {
+    const nameField = document.getElementById('product-name');
+    const name = nameField.value;
+    // display in UL
+    displayProduct(name);
+
+    // add to local storage
+
+    nameField.value = '';
+}
+
+const displayProduct = name => {
+    const ul = document.getElementById('products');
+    const li = document.createElement('li');
+    li.innerText = name;
+    ul.appendChild(li);
+}
